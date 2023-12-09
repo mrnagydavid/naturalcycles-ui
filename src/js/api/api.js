02da49da0f153f3cf3eeaa05c9b90f328e209044
mrnagydavid <mrnagydavid@gmail.com>
2023-12-09 15:07:19 +0100

    chore: set api url

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,9 +1,11 @@
 import { auth } from '../auth.js'
 
+const BASE_URL = 'https://mrnagydavid-nc-api-e8639114c055.herokuapp.com'
+
 export async function get(url) {
   const token = await getToken()
 
-  const response = await fetch(url, {
+  const response = await fetch(`${BASE_URL}${url}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -17,7 +19,7 @@ export async function get(url) {
 export async function put(url, body) {
   const token = await getToken()
 
-  const response = await fetch(url, {
+  const response = await fetch(`${BASE_URL}${url}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
--- src/js/api/profile.js
+++ src/js/api/profile.js
@@ -1,13 +1,11 @@
 import { get, put } from './api.js'
 
-const BASE_URL = 'http://localhost:3000'
-
 export async function fetchProfile() {
-  const { data } = await get(`${BASE_URL}/profile`)
+  const { data } = await get('/profile')
   return data
 }
 
 export async function updateProfile(params) {
-  const response = await put(`${BASE_URL}/profile`, params)
+  const response = await put('/profile', params)
   return response
 }
